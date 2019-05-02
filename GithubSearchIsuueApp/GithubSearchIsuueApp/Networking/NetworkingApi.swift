2f6de44dcf00014e9c02f95ddba91677a503150b
Jeongdasilver <alal8272@gmail.com>
2019-05-02 15:50:42 +0900

    [ADD] MVVM Logic

---
+++ GithubSearchIsuueApp/GithubSearchIsuueApp/Networking/NetworkingApi.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkingApi.swift
+//  GithubSearchIsuueApp
+//
+//  Created by daeun on 02/05/2019.
+//  Copyright © 2019 daeun. All rights reserved.
+//
+
+import Foundation
---
+++ GithubSearchIsuueApp/GithubSearchIsuueApp/Util/Base.swift
@@ -0,0 +1,9 @@
+//
+//  Base.swift
+//  GithubSearchIsuueApp
+//
+//  Created by daeun on 02/05/2019.
+//  Copyright © 2019 daeun. All rights reserved.
+//
+
+import Foundation
