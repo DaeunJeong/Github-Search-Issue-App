54a917a2d955a7afc141db283663219e4a18e9ba
Jeongdasilver <alal8272@gmail.com>
2019-05-02 15:36:01 +0900

    [INIT] init project

---
+++ GithubSearchIsuueApp/GithubSearchIsuueApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GithubSearchIsuueApp
+//
+//  Created by daeun on 02/05/2019.
+//  Copyright © 2019 daeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
