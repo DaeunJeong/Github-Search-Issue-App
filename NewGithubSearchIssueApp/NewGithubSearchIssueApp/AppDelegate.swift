ca5bbea8c8f8f9112ff8b06d05076bccddf83b23
Jeongdasilver <alal8272@gmail.com>
2019-06-21 20:16:57 +0900

    [DELETE] storyboard

--- NewGithubSearchIssueApp/NewGithubSearchIssueApp/AppDelegate.swift
+++ NewGithubSearchIssueApp/NewGithubSearchIssueApp/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow()
+        let rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = rootViewController
         return true
     }
 
